c88bb235ed6f5cef9b6758bf306e49b4d9394297
SaraMedhat450 <saramedh567@gmail.com>
2026-02-09 15:03:02 +0200

    remove navbar from customer page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Customer from '../pages/Customer';
 
 export default function MainLayout() {
   return (
     <>
-      <Navbar />
+      {!<Customer /> && <Navbar />}
       <Outlet />
     </>
   );
